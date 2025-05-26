a336d269f3ad457b046c50c7cbdc492728a137b8
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-05-26 17:41:52 +0200

    giving front to seb

---
+++ client/src/components/DetailedForm.tsx
@@ -0,0 +1,15 @@
+// choppe :form_id de l'url 
+// fetch les infos de formulaire pour form_id = xx
+
+function DetailedForm (){
+    // 3 value entre create / show /data
+return
+{/* <detail de la nav bar + chgmt valeur/> */}
+{/*puis switch case en foncion de la value nav bar
+cas1 : <CreateForm>
+cas2 : <AnswerForm> <= normalement, on n'a pas besoin de créer 
+cas3 : <Data:>*/ }
+
+}
+
+export default DetailedForm;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import './index.css'
 import App from './App.tsx'
+import Home from "./components/Home.tsx";
+import CreatorPage from "./components/CreatorPage.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/:user_id",
+        element: <CreatorPage />,
+      },
+      {
+        path: "/:user_id/:form_id",
+        element: <DetailedForm/>,
+      },
+      {
+        path: "/answer/:form_id",
+        element: <AnswerForm />,
+      },
+    ],
+  },
+]);
+
+const rootElement = document.getElementById("root");
+
+if (rootElement != null) {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
