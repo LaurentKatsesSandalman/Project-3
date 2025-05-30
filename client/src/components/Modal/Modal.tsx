80e10aa477b621a7550bfed753661dde9dc7b05b
Burdon Jordan <burdon.jordan@gmail.com>
2025-05-30 09:03:31 +0200

    save

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -3,25 +3,29 @@ import CloseIcon from "../../assets/icons/Icon-Close.svg";
 import styles from "./Modal.module.css";
 
 interface ModalProps {
-	children: React.ReactNode;
-	setActiveModal: AppContextType["setIsSignUpActive"];
+    children: React.ReactNode;
+    setActiveModal: AppContextType["setIsSignUpActive"];
 }
 
 function Modal({ children, setActiveModal }: ModalProps) {
-	return (
-		<div className={styles.modalContainer}>
-			<div className={styles.modal}>
-				{children}
-				<button
-					className={styles.closeBtn}
-					onClick={() => setActiveModal(false)}
-					type="button"
-				>
-					<img className={styles.closeIcon} src={CloseIcon} alt="Close Icon" />
-				</button>
-			</div>
-		</div>
-	);
+    return (
+        <div className={styles.modalContainer}>
+            <div className={styles.modal}>
+                {children}
+                <button
+                    className={styles.closeBtn}
+                    onClick={() => setActiveModal(false)}
+                    type="button"
+                >
+                    <img
+                        className={styles.closeIcon}
+                        src={CloseIcon}
+                        alt="Close Icon"
+                    />
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default Modal;
--- client/src/components/SignUp/SignUp.tsx
+++ client/src/components/SignUp/SignUp.tsx
@@ -1,9 +1,7 @@
-import styles from "./SignUp.module.css"
+import styles from "./SignUp.module.css";
 
 function SignUp() {
-    return (
-        <div>CREER UN COMPTE</div>
-    )
+    return <div>CREER UN COMPTE</div>;
 }
 
 export default SignUp;
