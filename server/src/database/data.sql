77c272826954ae4569f785c42f5f8230a06b5fac
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-06-02 16:03:07 +0200

    real data for field type / fake data for field, field option, form, theme

---
+++ server/src/database/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO field_type (id, type) VALUES
+(1, 'text'),
+(2, 'checkbox'),
+(3, 'date'),
+(4, 'email'),
+(5, 'month'),
+(6, 'number'),
+(7, 'radio'),
+(8, 'tel'),
+(9, 'url'),
+(10, 'time'),
+(11, 'textarea'),
+(12, 'droplist');
---
+++ server/src/database/mockdata/form_i.sql
@@ -0,0 +1,3 @@
+INSERT INTO form (is_deployed, is_closed, creation_date, is_public, multi_answer, theme_id, user_id) VALUES
+(1,0,"2025-06-01 00:00:01",0,0,1,1),
+(1,0,"2025-06-02 00:01:01",0,0,1,2);
