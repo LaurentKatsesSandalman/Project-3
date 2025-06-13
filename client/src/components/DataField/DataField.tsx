c8d8131e30abf1a4b442458e33dbb6567c8543a6
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-06-13 17:07:08 +0200

    bad code for DataField quick version + need to create 3 more components FlatAnswers, BarAnswers, CheeseAnswers

--- client/src/components/DataField/DataField.tsx
+++ client/src/components/DataField/DataField.tsx
@@ -1,14 +1,35 @@
 import type { Field } from "../../types/field"
+import { useState } from "react"
 
 interface DataFieldProps{
     field: Field
 }
 
 function DataField({field}:DataFieldProps){
+// je ne vois pas comment faire le mapping en fonction du type dans datatab
+// (car le type de display aggregate n'est pas exactement le même que le type de display answer)
+// donc je le fais ici
+const [fieldDisplay, setFieldDisplay] = useState< "flat"|"bar"|"cheese">("flat")
 
+switch (field.field_type_id)
+{
+    case 1 : {
+        setFieldDisplay("flat")
+        break;
+    }
+    case 2 : {
+        setFieldDisplay("bar")
+        break;
+    }
+}
+    
 
     return(<>
-    <p>Pipo data field</p>
+    <p>{field.name}</p>
+    <p>{field.description}</p>
+    {fieldDisplay==="flat"&&<FlatAnswers/>}
+    {fieldDisplay==="bar"&&<BarAnswers/>}
+    {fieldDisplay==="cheese"&&<CheeseAnswers/>}
     </>)
 }
 
