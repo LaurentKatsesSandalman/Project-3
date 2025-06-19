881db681689406ca6128afadf30927d3ceabaf8a
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-06-19 16:17:46 +0200

    comment in PR on err.response?.status === 403 || err.response?.status === 401

--- client/src/components/FormCreator/FormCreator.tsx
+++ client/src/components/FormCreator/FormCreator.tsx
@@ -88,7 +88,7 @@ const FormCreator = () => {
             setForm(response.data);
         } catch (err: any) {
             // When there is an issue with the token
-            if (err.status === 403 || err.status === 401) {
+            if (err.response?.status === 403 || err.response?.status === 401) {
                 setAuthToken(null);
             }
         }}
