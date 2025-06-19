31bf4c21edf582b736be420517007b9cbe8aff13
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-06-19 17:41:54 +0200

    field desc cannot be null anymore

--- client/src/types/fields.ts
+++ client/src/types/fields.ts
@@ -17,7 +17,7 @@ export interface Field {
     field_id: number;
     field_ordering: number;
     field_name: string;
-    field_description: string | null;
+    field_description: string;
     default_value: string | null;
     is_required: boolean;
     is_unique: boolean;
