3c4acf0d5c09c00f83f9accc52769fbc30fb9503
Burdon Jordan <burdon.jordan@gmail.com>
2025-06-20 11:58:25 +0200

    ÃRemoved more comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import { AppProvider } from "./context/AppContext";
 import TopBar from "./components/TopBar/TopBar";
 import "./App.css";
-import CreatorPage from "./pages/CreatorPage/CreatorPage";
 
 function App() {
     return (
@@ -10,7 +9,6 @@ function App() {
             <TopBar />
             <main>
                 <Outlet />
-                {/* <CreatorPage /> */}
             </main>
         </AppProvider>
     );
