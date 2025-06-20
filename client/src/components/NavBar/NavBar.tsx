9394d976f3e9e1330b7fb0de09f245c767581a94
7NoGameNoLife7 <sebastien.zllegros@gmail.com>
2025-06-20 11:02:03 +0200

    truc commit

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,37 +1,42 @@
 
- 
- <div className={styles['mode-toggle-buttons']}>Add commentMore actions
-        <button
-          type="button"
-          onClick={() => setViewMode('edit')}
-          className={`${styles['mode-toggle-button']} ${viewMode === 'edit' ? styles['active'] : ''}`}
-        >
-          Éditer
-        </button>
-        <button
-          type="button"
-          onClick={() => setViewMode('preview')}
-          className={`${styles['mode-toggle-button']} ${viewMode === 'preview' ? styles['active'] : ''}`}
-        >
-          Aperçu
-        </button>
-        <button
-          type="button"
-          onClick={() => setViewMode('result')}
-          className={`${styles['mode-toggle-button']} ${viewMode === 'result' ? styles['active'] : ''}`}
-        >
-          Résultat
-        </button>
-      </div>
+ function navBar () {
+    return;
+    <div className={styles['mode-toggle-buttons']}>Add commentMore actions
+            <button
+              type="button"
+              onClick={() => setViewMode('edit')}
+              className={`${styles['mode-toggle-button']} ${viewMode === 'edit' ? styles['active'] : ''}`}
+            >
+              Éditer
+            </button>
+            <button
+              type="button"
+              onClick={() => setViewMode('preview')}
+              className={`${styles['mode-toggle-button']} ${viewMode === 'preview' ? styles['active'] : ''}`}
+            >
+              Aperçu
+            </button>
+            <button
+              type="button"
+              onClick={() => setViewMode('result')}
+              className={`${styles['mode-toggle-button']} ${viewMode === 'result' ? styles['active'] : ''}`}
+            >
+              Résultat
+            </button>
+          </div>
 
-      {viewMode === 'preview' ? (
-        <FormPreview
-          formTitle={formTitle}
-          formDescription={formDescription}
-          formFields={formFields}
-        />
-      ) : viewMode === 'result' ? (
-        <div className={styles['result-page']}>
-          {/* Page blanche pour les résultats pour le moments a changer pour les vrai result*/}
-        </div>
-      ) : (
+          {viewMode === 'preview' ? (
+            <FormPreview
+              formTitle={formTitle}
+              formDescription={formDescription}
+              formFields={formFields}
+            />
+          ) : viewMode === 'result' ? (
+            <div className={styles['result-page']}>
+              {/* Page blanche pour les résultats pour le moments a changer pour les vrai result*/}
+            </div>
+          ) : (
+
+}
+
+export default navBar;
